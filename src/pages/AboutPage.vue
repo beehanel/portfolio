9c28fdeaf567efaa56d2472f9646acd076013d41
Swim Ouarda <ouardasaraines@gmail.com>
2025-12-04 15:48:15 -0500

    Added about me page and diacritics temporary removel

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="page about-page">
     <h2>About</h2>
-
-	  <p>
-		  Page in progress
-	  </p>
+			  <p class="tagline">
+				I am a web and software developer with an interest in educational technology, game development, audio, and machine learning. I am currently on a contract with McGill University until the end of December 2025. I'm looking for my next opportunity in software development.
+			  </p>
+			  <div class="two-col">
+				  <div>
+					  <p>
+						  Programming Skills:
+					  </p>
+					  <ul>
+						  <li>HTML/CSS/Javascript</li>
+						  <li>C#</li>
+						  <li>Python</li>
+						  <li>Machine Learning/NLP</li>
+					  </ul>
+				  </div>
+				  <div>
+					  <p>
+						  Tools
+					  </p>
+					  <ul>
+						  <li>Unity</li>
+						  <li>Adobe Photoshop & Photopea</li>
+						  <li>Autodesk Maya & Tinkercad</li>
+						  <li>Jira</li>
+						  <li>GitHub</li>
+						  <li>FMOD/FL Studio</li>
+					  </ul>
+				  </div>
+			  </div>
     <h3>
       Demo Reel 2023
     </h3>
